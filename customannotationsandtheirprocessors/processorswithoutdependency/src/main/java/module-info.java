a49da4946b6fdffc7c614ed8f6b406bbe1311000
Aria Adibi <25481147+AriaAdibi@users.noreply.github.com>
2022-03-07 13:55:40 -0500

    Init:: Testing Annotation Processors

---
+++ customannotationsandtheirprocessors/processorswithoutdependency/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+import processors.IterativeSimpleBareProcessor;
+
+module processorswithoutdependency {
+  requires annotations; // nonstatic ... for element.getAnnotation() core reflection ...
+  requires static com.google.auto.service;
+  requires static com.squareup.javapoet;
+
+  requires static java.compiler;
+  provides javax.annotation.processing.Processor with IterativeSimpleBareProcessor;
+}
---
+++ tests/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module tests {
+  requires static annotations;
+
+//  requires static java.compiler;
+//  uses javax.annotation.processing.Processor;
+}
