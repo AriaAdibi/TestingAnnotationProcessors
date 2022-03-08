d6771cc0a3ee3d1851bdbb89adb09a0965d15328
Aria Adibi <aria.adibi@nakisa.com>
2022-03-08 09:55:10 -0500

    Flattened the Structure

--- processorswithoutdependency/src/main/java/module-info.java
+++ processorswithoutdependency/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module processorswithoutdependency {
   requires static annotations;
   requires static com.google.auto.service;
-  requires static java.compiler;
   requires static com.squareup.javapoet;
+  requires static java.compiler;
+  provides javax.annotation.processing.Processor with processors.IterativeSimpleBareProcessor;
 }
